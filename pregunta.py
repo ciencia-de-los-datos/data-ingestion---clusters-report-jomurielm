2d30b84b650baaa4bc16cedd094e5e2e63482cf6
jomurielm <144398722+jomurielm@users.noreply.github.com>
2023-10-04 23:29:12 -0500

    Entrega total del laboratorio propuesto

--- pregunta.py
+++ pregunta.py
@@ -10,12 +10,23 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
-
+import re
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df =  pd.read_fwf("clusters_report.txt", widths = [9, 16, 16, 80],header=None,
+                    names = ["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave", "principales_palabras_clave"],
+                    converters = {"porcentaje_de_palabras_clave": lambda x: x.rstrip(" %").replace(",",".")}
+                    ).drop(index={0,1,2}).ffill()
 
+    df = df.astype  ({ "cluster": int, 
+                    "cantidad_de_palabras_clave": int, 
+                    "porcentaje_de_palabras_clave": float,
+                    "principales_palabras_clave": str
+                    })
+
+    df = df.groupby(["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave"])["principales_palabras_clave"].apply(lambda x: ' '.join(map(str,x))).reset_index()
+    df["porcentaje_de_palabras_clave"] = df["porcentaje_de_palabras_clave"].apply(lambda x: round(x,1))
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].apply(lambda x: re.sub(r'\s+', ' ',x).rstrip("."))
+    
     return df
